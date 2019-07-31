e5b8e817ceb2dc975967d74d17bdabf484924936
Virendersingh Gulair <Virendersingh_Gulair@epam.com>
2019-07-31 17:26:08 +0530

    Task done.

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 function dis(val) {
     document.getElementById("answer").value += val
+    document.getElementById("label").innerHTML = document.getElementById("answer").value
 }
 
 //function that evaluates the digit and return answer 
@@ -7,9 +8,14 @@ function solve() {
     let x = document.getElementById("answer").value
     let y = eval(x)
     document.getElementById("answer").value = y
+    let result = document.createElement("div")
+    result.setAttribute("id", "result")
+    result.appendChild(document.createTextNode("Ans : " + y))
+    document.getElementById("label").appendChild(result)
 }
 
 //function that clear the display 
 function clr() {
     document.getElementById("answer").value = ""
+    document.getElementById("label").innerHTML = ""
 }
